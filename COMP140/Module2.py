0c05c2d1eb47d79bafa43b1d37a8f4efa4f796e6
sj43 <sj43@rice.edu>
2018-09-05 13:18:43 -0500

    update!

--- COMP140/Module2.py
+++ COMP140/Module2.py
@@ -1,14 +1,3 @@
 """
 COMP140 : Module 2 - Spot It!
 """
-old_tuple = (2,5,7,10)
-mylist = list(old_tuple)
-
-mylist.reverse()
-
-new_tuple = tuple(mylist)
-
-print (old_tuple)
-print (new_tuple)
-print("another one")
-print("third one")
