824beb3ef71f29b600654d0e6688301966f1a9dc
sj43 <sj43@rice.edu>
2018-09-08 14:49:43 -0500

    updateeee

--- Hackerrank/PrintPretty.cpp
+++ Hackerrank/PrintPretty.cpp
@@ -3,21 +3,20 @@
 using namespace std;
 
 int main() {
-	int T; cin >> T;
-	cout << setiosflags(ios::uppercase);
-	cout << setw(0xf) << internal;
-	while(T--) {
-		double A; cin >> A;
-		double B; cin >> B;
-		double C; cin >> C;
-		/* Enter your code here */
-		cout<<hex<<left<<showbase<<nouppercase;
-		cout<< (long long)A <<endl;
-		cout<<dec<<right<<setw(15)<<setfill('_')<<showpos<<fixed<<setprecision(2);
-		cout<<B<<endl;
-		cout<<scientific<<uppercase<<noshowpos<<setprecision(9);
-		cout<<C<<endl;
-        cout<<"this is subbranch"<<endl;
-	}
-	return 0;
+        int T; cin >> T;
+        cout << setiosflags(ios::uppercase);
+        cout << setw(0xf) << internal;
+        while(T--) {
+                double A; cin >> A;
+                double B; cin >> B;
+                double C; cin >> C;
+                /* Enter your code here */
+                cout<<hex<<left<<showbase<<nouppercase;
+                cout<< (long long)A <<endl;
+                cout<<dec<<right<<setw(15)<<setfill('_')<<showpos<<fixed<<setprecision(2);
+                cout<<B<<endl;
+                cout<<scientific<<uppercase<<noshowpos<<setprecision(9);
+                cout<<C<<endl;
+        }
+        return 0;
 }
