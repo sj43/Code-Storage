19fb15386694dc7d25e4f0d2c3c2e51ea5c8bdf5
sj43 <sj43@rice.edu>
2018-09-12 01:03:07 -0500

    commiting

--- Hackerrank/AccessingInheritedFunctions.cpp
+++ Hackerrank/AccessingInheritedFunctions.cpp
@@ -3,91 +3,104 @@ using namespace std;
 
 class A {
 public:
-A() {
-	callA = 0;
-}
-
+	A() {
+		callA = 0;
+	}
 private:
-int callA;
-void inc() {
-	callA++;
-}
-
+	int callA;
+	void inc() {
+		callA++;
+	}
 protected:
-void func(int &a) {
-	a = a * 2;
-	inc();
-}
-
+	void func(int &a) {
+		a = a * 2;
+		inc();
+	}
 public:
-int getA() {
-	return callA;
-}
+	int getA() {
+		return callA;
+	}
 };
 
 class B {
 public:
-B() {
-	callB = 0;
-}
+	B() {
+		callB = 0;
+	}
 
 private:
-int callB;
-void inc() {
-	callB++;
-}
+	int callB;
+	void inc() {
+		callB++;
+	}
 
 protected:
-void func(int &a) {
-	a = a * 3;
-	inc();
-}
+	void func(int &a) {
+		a = a * 3;
+		inc();
+	}
 
 public:
-int getB() {
-	return callB;
-}
+	int getB() {
+		return callB;
+	}
 };
 
 class C {
 public:
-C() {
-	callC = 0;
-}
+	C() {
+		callC = 0;
+	}
 
 private:
-int callC;
-void inc() {
-	callC++;
-}
+	int callC;
+	void inc() {
+		callC++;
+	}
 
 protected:
-void func(int &a) {
-	a = a * 5;
-	inc();
-}
+	void func(int &a) {
+		a = a * 5;
+		inc();
+	}
 
 public:
-int getC() {
-	return callC;
-}
+	int getC() {
+		return callC;
+	}
 };
 
-class D {
+class D : public A, public B, public C {
 
-int val;
+	int val;
 
 public:
 // Initially val is 1
-D() {
-	val = 1;
-}
+	D() {
+		val = 1;
+	}
 
 // Implement this function
-void update_val(int new_val) {
-}
+	void update_val(int new_val) {
+		int a = new_val;
+		while ( a %2 == 0)
+		{
+			a = a/2;
+			A::func(val);
+		}
+		while ( a % 3 == 0)
+		{
+			a = a/3;
+			B::func(val);
+		}
+		while ( a % 5 == 0)
+		{
+			a = a/5;
+			C::func(val);
+		}
+	}
 // For Checking Purpose
-void check(int);           // Do not delete this line.
+	void check(int);                       // Do not delete this line.
 };
 
 void D::check(int new_val) {
