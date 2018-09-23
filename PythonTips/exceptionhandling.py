39342a1a60f62b3337ec543e4f657bf3eb0075a0
sj43 <sj43@rice.edu>
2018-09-22 20:37:24 -0500

    9/22 python studying

--- PythonTips/exceptionhandling.py
+++ PythonTips/exceptionhandling.py
@@ -1,10 +1,10 @@
 try:
-    file = open('test.txt','rb')
+    file = open('test.txt', 'rb')
 except (IOError, EOFError) as e:
     print('An IOError occured. {}'.format(e.args[-1]))
 
 
 try:
-    file = open('test.txt','rb')
+    file = open('test.txt', 'rb')
 except Exception:
     raise
