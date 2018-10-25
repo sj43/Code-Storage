ee1779a6a4697521ab06b9d230a05ecfc59f9fa6
sj43 <sj43@rice.edu>
2018-10-25 00:41:30 -0500

    10/25

--- BaekJoon/2293.cpp
+++ BaekJoon/2293.cpp
@@ -1,7 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;
-int a[101];
-int main(){
-  
-  return 0;
+#include <cstdio>
+int main() {
+    int n, k;
+    int coins[101];
+    int d[10001] = {0};
+    scanf("%d %d", &n, &k);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &coins[i]);
+    }
+    d[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (j >= coins[i]) {
+                d[j] += d[j-coins[i]];
+            }
+        }
+    }
+    printf("%d\n", d[k]);
+    return 0;
 }
