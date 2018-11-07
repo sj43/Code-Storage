94ce48d5302a65b5a253e33dae172012257ac654
sj43 <sj43@rice.edu>
2018-11-07 16:26:54 -0600

    day7

---
+++ DailyCodingProblem/day7.py
@@ -0,0 +1,21 @@
+# This problem was asked by Facebook.
+#
+# Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+#
+# For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+#
+# You can assume that the messages are decodable. For example, '001' is not allowed.
+
+def num_encodings(s):
+    if s.startswith('0'):
+        return 0
+    elif len(s) <= 1: # This covers empty string
+        return 1
+
+    total = 0
+
+    if int(s[:2]) <= 26:
+        total += num_encodings(s[2:])
+
+    total += num_encodings(s[1:])
+    return total
