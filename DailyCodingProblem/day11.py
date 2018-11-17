da76aa19da4c28ac6822f504ee24475c1402aef5
sj43 <sj43@rice.edu>
2018-11-17 13:33:37 -0600

    daily coding problem

--- DailyCodingProblem/day11.py
+++ DailyCodingProblem/day11.py
@@ -29,3 +29,37 @@ class Trie(object):
                 trie[char] = {}
             trie = trie[char]
         trie[ENDS_HERE] = True
+
+    def elements(self, prefix):
+        d = self._trie
+        for char in prefix:
+            if char in d:
+                d = d[char]
+            else:
+                return []
+        return [prefix + x for x in self._elements(d)]
+
+    def _elements(self, d):
+        result = []
+        for c, v in d.items():
+            if c == ENDS_HERE:
+                subresult = ['']
+            else:
+                subresult = [c + s for s in self._elements(v)]
+            result.extend(subresult)
+        return result
+
+    def print_trie(self):
+        print(self._trie)
+
+
+trie = Trie()
+for word in WORDS:
+    trie.insert(word)
+
+def autocomplete(s):
+    return trie.elements(s)
+
+# trie.print_trie()
+
+print(autocomplete('de'))
