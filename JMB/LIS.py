ed2db29790a9c4e5c602639bfcd8dbc66160dac0
sj43 <sj43@rice.edu>
2018-12-30 16:12:08 +0900

    Update LIS.py

--- JMB/LIS.py
+++ JMB/LIS.py
@@ -53,38 +53,3 @@
 ##    cache = {}
 ##    S_ = map(int, raw_input().strip().split())
 ##    print lis3(-1) - 1
-
-
-
-
-
-
-# function lis4 - binary search
-def lst4(num, lis_idx):
-    if lis_idx == 0 or (lis_idx > 0 and cache[lis_idx - 1] <= num):
-        cache[lis_idx] = num
-        lis_idx += 1
-        return lis_idx
-
-    front = 0
-    rear = lis_idx - 1
-    # binary search
-    while front <= rear:
-        mid = (front + rear) / 2
-        if cache[mid] < num:
-            front = mid + 1
-        else:
-            rear = mid - 1
-    cache[rear + 1] = num
-    return lis_idx
-
-
-for _ in xrange(input()):
-    N = input()
-    cache = {}
-    S = map(int, raw_input().strip().split())
-    idx = 0
-    for i in xrange(N):
-        idx = lst4(S[i], idx)
-    print idx
-
