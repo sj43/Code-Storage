fa57a3c5f740b3b41e8abee17dfce2eb404e81cb
sj43 <sj43@rice.edu>
2019-04-28 20:58:33 -0500

    Struct Node

--- ChewC/StructNode/main.c
+++ ChewC/StructNode/main.c
@@ -8,5 +8,8 @@ int main(){
   struct Node* node2 = InsertNode(node1, 300);
   struct Node* node3 = InsertNode(node1, 400);
   PrintNodeFrom(root);
+  DestroyNode(node1, root);
+  printf("\n");
+  PrintNodeFrom(root);
   return 0;
 }
